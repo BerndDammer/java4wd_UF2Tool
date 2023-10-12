f67ea5b3c62bf9d17ca5aa3f62c75166ceb1e274
Bernd Dammer <Bernd.Dammer@web.de>
2023-10-12 13:50:07 +0200

    debug at dev station

---
+++ compile_for_dev_station.cmd
@@ -0,0 +1,4 @@
+call ..\..\SetEnv.bat
+rem mvn clean package -Djavafx.platform=linux-monocle
+mvn clean package -Djavafx.platform=linux-arm32-monocle
+pause -----------------------pc-----------------------
---
+++ copy_to_dev_station.cmd
@@ -0,0 +1,3 @@
+call ..\..\SetEnv.bat
+scp target/UF2Tool-0.2.1-DEV-jar-with-dependencies.jar aeningning@aewinter.flat:~/tyra.jar
+pause -----------------------pc-----------------------
