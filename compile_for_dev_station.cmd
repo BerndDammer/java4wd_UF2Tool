af511227a441232d4c884bb9c3ef6100a088319f
Bernd Dammer <Bernd.Dammer@web.de>
2023-10-12 19:26:41 +0200

    run trys at raspi and x11

--- compile_for_dev_station.cmd
+++ compile_for_dev_station.cmd
@@ -1,4 +1,5 @@
 call ..\..\SetEnv.bat
 rem mvn clean package -Djavafx.platform=linux-monocle
 mvn clean package -Djavafx.platform=linux-arm32-monocle
+rem mvn clean package -Djavafx.platform=linux-aarch64
 pause -----------------------pc-----------------------
--- copy_to_dev_station.cmd
+++ copy_to_dev_station.cmd
@@ -1,3 +1,4 @@
 call ..\..\SetEnv.bat
 scp target/UF2Tool-0.2.1-DEV-jar-with-dependencies.jar aeningning@aewinter.flat:~/tyra.jar
+scp javafx.platform.properties aeningning@aewinter.flat:~
 pause -----------------------pc-----------------------
